0e1de43435e262a78c92c63fb43cae4a1c770dec
Amer Mall <amer@icloud.com>
2021-11-28 16:44:09 -0500

    commit tree 8e1e6afac58a25d5e339d0dcae7f42b81fe5f9c8

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+// Implement a function called saturdayFun. It should return a String like "This Saturday, I want to ....!" Fill in the ... with the activity that's passed in as the first parameter. If nothing is passed in, default to "roller-skate". Run learn to verify you've gotten the first set of tests passing before continuing with the lesson.
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
 }
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+// Implement a function expression called mondayWork. The function should return a String like "This Monday, I will ... ." Fill in the ... with the activity that's passed in as the first parameter. If nothing is passed in, default to "go to the office". Run learn to verify you've gotten this set of tests passing before continuing with the lesson.
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+// Implement a function called wrapAdjective:
+
+// - It should return a function
+// -- This "inner" function should:
+// --- take a single parameter that should default to "special". Name it however you wish.
+// --- return a String of the form "You are ..." where ... should be the adjective this function received wrapped in visual flair
+
+// - It should take as parameter a String that will be used to create visual flair
+// - You may call the parameter whatever you like, but its default value should be "*"
+//  - Call example: const encouragingPromptFunction = wrapAdjective("!!!")
+
+function wrapAdjective(flair = "*") {
+  const innerFunction = function(state = "special") {
+    return `You are ${flair}${state}${flair}!`;
   }
+  return innerFunction;
 }
+
